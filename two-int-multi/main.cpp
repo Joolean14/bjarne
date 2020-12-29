7ea9c49860ee0ede95d1184392ba8550147be677
julian <Joolean14>
2020-12-29 08:10:05 -0500

    Began working on Chapter 1 exercises

---
+++ two-int-multi/main.cpp
@@ -0,0 +1,11 @@
+// Write a program that prompts the user to enter two integer values . Store
+// these values in int variables  named val1 and val2. Write your program to 
+// determine the smallest, largest, sum, difference, product and ratio of these
+// values and report them to the user.
+
+#include <iostream>
+
+int main () {
+    b
+    return 0;
+}
