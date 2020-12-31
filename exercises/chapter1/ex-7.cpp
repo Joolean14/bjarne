c260447cf0c43d22368d00b5c30928325bd1bfc7
julian <Joolean14>
2020-12-31 14:29:19 -0500

    Resuelvo ejercicio 7 pero quedo con muchas dudas aun

--- exercises/chapter1/ex-7.cpp
+++ exercises/chapter1/ex-7.cpp
@@ -2,24 +2,52 @@
 // values Steinbeck, Hemingway, Fitzgerald, the output should be Fitzgerald,
 // Hemingway, Steinbeck.
 
+#include <bits/stdc++.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
+void printToConsole(std::vector<std::string> names) {
+  for (int i = 0; i < names.size(); i++)
+    std::cout << names[i] << "\n";
+}
+
+bool compare(std::string a, std::string b) {
+  // returns 1 if string a is alphabetically less than string b
+  return a < b;
+}
+
+std::vector<std::string> alphabeticallySort(std::vector<std::string> a) {
+  int n = a.size();
+  sort(a.begin(), a.end(), compare);
+  return a;
+}
+
 int main() {
   std::vector<std::string> names;
-  std::string name1 = "";
+  std::string inputName1 = "";
+  std::string inputName2 = "";
+  std::string inputName3 = "";
+
   std::cout << "Ingrese un nombre\n";
-  std::cin >> name1;
-  names.push_back(name1);
-  for (int x : names) {
-    std::cout << x << " ";
-  }
+  std::cin >> inputName1;
+  names.push_back(inputName1);
+  std::cout << "Ingrese segundo nombre\n";
+  std::cin >> inputName2;
+  names.push_back(inputName2);
+  std::cout << "Ingrese tercer nombre\n";
+  std::cin >> inputName3;
+  names.push_back(inputName3);
+
+  names = alphabeticallySort(names);
+  std::cout << "Alphabetically sorted inputs\n";
+  printToConsole(names);
+
   return 0;
 }
 
 // Create a vector
 // Implement sort()
+// How to cout a vector?
+// strcmp
 // https://www.includehelp.com/cpp-programs/sort-names-in-an-alphabetical-order.aspx
-// Como imprimo el vector?
- 
